4cc09ec87e0114d17c5600135697b7398cdb4bbf
Zhehao Lu <zhehaolu@usc.edu>
2018-03-31 19:58:45 -0700

    Finished the API for price fetcher.

--- priceJS.js
+++ priceJS.js
@@ -1,31 +1,26 @@
 /**
  * Functions that make requests to the uber and lyft APIs for prices
+ * Lyft key is ranlated by https://github.com/kigiri/fetch, original client_id and client_secret not listed
  */
-
+const lyftKey = 'dURUWndGcUxsSnJwOkRVdDVDUXI4bkRUSWhzNjFtWHZpY3FBMUFrMFhZdWFF'
 let accessToken = {
 	lyft: '',
 	uber: '-Q_I4XWhcADPx5e2YXUJQnndK2Cs6ugTM9_HkdJA'
 }
 
-/**
- * Tranlated by https://github.com/kigiri/fetch, original client_id and client_secret not listed
- */
-const lyftKey = 'dURUWndGcUxsSnJwOkRVdDVDUXI4bkRUSWhzNjFtWHZpY3FBMUFrMFhZdWFF'
-
-/**
- * Fetch the newest lyft token. Uber's server token is ready
- */
-function setUp() {
+export default function retrieveData(startLat, startLng, endLat, endLng) {
+	//Fetch the newest lyft token. Uber's server token is ready
 	fetchLyftToken()
-		.then(data => lyftTokenHandler(data))
-		.catch(error => console.error(error));
+		.then(data => { lyftTokenHandler(data);
+			fetchLyftPrice(startLat, startLng, endLat, endLng)
+		});
+	fetchUberPrice(startLat, startLng, endLat, endLng);
 }
 
 /**
  * Use .then to get access to the data post-fetched
  */
 function fetchLyftToken() {
-	console.log("Start fetching");
 	return fetch("https://api.lyft.com/oauth/token", {
 		body: JSON.stringify({"grant_type": "client_credentials", 
 			"scope": "public"}),
@@ -49,20 +44,60 @@ function fetchLyftPrice(startLat, startLng, endLat, endLng) {
 			Authorization: "Bearer " + accessToken.lyft
 		}
 	})
-		.then(response => response.json());
+		.then(response => response.json())
+		.then(data => lyftEvent(data));
 }
 
 function fetchUberPrice(startLat, startLng, endLat, endLng) {
-	let URL = "https://api.uber.com/v1/estimates/price?start_latitude=" + String(startLat) + "&start_longitude=" + String(startLng) + "&end_latitude=" + String(endLat) + "&end_longitude=" + String(endLng);
-	//URL += ("&server_token=" + accessToken.uber);
-	console.log(URL);
+	let url = "https://api.uber.com/v1/estimates/price?start_latitude=" + String(startLat) + "&start_longitude=" + String(startLng) + "&end_latitude=" + String(endLat) + "&end_longitude=" + String(endLng);
+	//url += ("&server_token=" + accessToken.uber);
+	console.log(url);
 	
-	return fetch(URL, {
+	fetch(url, {
 		headers: {
 			Authorization: 'Token -Q_I4XWhcADPx5e2YXUJQnndK2Cs6ugTM9_HkdJA',
 			'Content-Type': 'application/json',
 			'Accept-Language': 'en_US'
 		}
 	})
-		.then(response => response.json());
+		.then(response => response.json())
+		.then(data => uberEvent(data));
+}
+
+/**
+ * Custom event handlers and listeners
+ */
+
+const lyftType = ['lyft_line', 'lyft', 'lyft_plus'];
+const uberType = ['POOL', 'uberX', 'uberXL'];
+
+function lyftEvent(data) {
+	function typeMatch(type) {
+		return lyftType.includes(type.ride_type);
+	}
+	let parse = data.cost_estimates.filter(typeMatch)
+	let eventData = { data: [] };
+	parse.forEach(function(item) {
+		eventData.data.push({'type': item.ride_type, 'estimatedPrice': item.estimated_cost_cents_min})
+	});
+	console.log(eventData);
+	let lyftPrice = new CustomEvent("lyftPrice", eventData);
+	document.dispatchEvent(lyftPrice);
+}
+
+function uberEvent(data) {
+	function typeMatch(type) {
+		return uberType.includes(type.localized_display_name);
+	}
+	let parse = data.prices.filter(typeMatch);
+	let eventData = { data: [] };
+	parse.forEach(function(item) {
+		eventData.data.push({'type': item.localized_display_name,
+			'estimatedPrice': item.low_estimate,
+			'estimatedPrice_high': item.high_estimate	
+		})
+	})
+	console.log(eventData);
+	let lyftPrice = new CustomEvent("uberPrice", eventData);
+	document.dispatchEvent(lyftPrice);
 }
