881f19dcd849b19e3ea7139706c2608cfb30efd7
Eric Y <eye@usc.edu>
2018-05-28 23:18:16 -0700

    Made Uber and Lyft async/await instead of onEvent and dispatchEvent

--- public/javascripts/priceJS.js
+++ public/javascripts/priceJS.js
@@ -3,36 +3,47 @@ import {EventTarget} from 'EventTarget.js'
 var uberTarget = new EventTarget();
 var lyftTarget = new EventTarget();
 
-function retrieveData(startLat, startLng, endLat, endLng) {
-	fetchLyftPrice(startLat, startLng, endLat, endLng);
-	fetchUberPrice(startLat, startLng, endLat, endLng);
+async function retrieveData(startLat, startLng, endLat, endLng) {
+	const lyftPrices = fetchLyftPrice(startLat, startLng, endLat, endLng);
+	const uberPrices = fetchUberPrice(startLat, startLng, endLat, endLng);
+	await lyftPrices;
+	await uberPrices;
 }
 
 
-function fetchLyftPrice(startLat, startLng, endLat, endLng) {
+async function fetchLyftPrice(startLat, startLng, endLat, endLng) {
 	let url = "https://api.lyft.com/v1/cost?start_lat=" + String(startLat) + "&start_lng=" + String(startLng) + "&end_lat=" + String(endLat) + "&end_lng=" + String(endLng);
 	//let url = "https://api.lyft.com/v1/cost?start_lat=" + "34.0689254" + "&start_lng=" + "-118.4473698" + "&end_lat=" + String(endLat) + "&end_lng=" + String(endLng);
-	fetch('/searchLyft', {
-		method: "GET",
-		headers: {
-			url: url
-		}
-	})
-		.then(response => response.json())
-		.then(data => {console.log(data);lyftEvent(data)});
+	try{
+		const response = fetch('/searchLyft', {
+			method: "GET",
+			headers: {
+				url: url
+			}
+		});
+		var data = await response.json();
+		console.log(data);
+		lyftEvent(data);
+	}catch(err) {
+		console.log('lyft fetch failed', err);
+	}
 }
 
 function fetchUberPrice(startLat, startLng, endLat, endLng) {
 	let url = "https://api.uber.com/v1.2/estimates/price?start_latitude=" + String(startLat) + "&start_longitude=" + String(startLng) + "&end_latitude=" + String(endLat) + "&end_longitude=" + String(endLng);
-
-	fetch('/searchUber', {
-		method: 'GET',
-		headers: {
-			url: url
-		}
-	})
-		.then(response => response.json())
-		.then(data => {console.log(data);uberEvent(data)});
+	try{
+		const response = fetch('/searchUber', {
+			method: 'GET',
+			headers: {
+				url: url
+			}
+		});
+		var data = await response.json();
+		console.log(data);
+		uberEvent(data);
+	}catch(err){
+		console.log('lyft fetch failed', err);
+	}
 }
 
 /**
@@ -67,8 +78,7 @@ function lyftEvent(data) {
 		})
 	});
 	eventData.detail.sort(compare);
-	let lyftPrice = new CustomEvent("lyftPrice", eventData);
-	lyftTarget.dispatchEvent(lyftPrice);
+	return eventData
 }
 
 function uberEvent(data) {
@@ -88,6 +98,29 @@ function uberEvent(data) {
 	})
 	console.log(parse);
 	eventData.detail.sort(compare);
-	let uberPrice = new CustomEvent("uberPrice", eventData);
-	uberTarget.dispatchEvent(uberPrice);
+	return eventData;
+}
+
+function resolveUber(data){
+	let prices = data.detail;
+	console.log(prices);
+	let x = document.getElementById("uberinfo");
+	let text = '';
+	prices.forEach(function (item) {
+		console.log(item);
+		text += ('<div class="datarow lead">' + typeToName[item.key] + '<span class="price">' + String(item.value.estimatedPrice) + '-' + String(item.value.estimatedPrice_high) + '</span></div/>');
+	});
+	x.innerHTML = text;
+}
+
+function resolveLyft(data){
+	let prices = data.detail;
+	console.log(prices);
+	let x = document.getElementById("lyftinfo");
+	let text = '';
+	prices.forEach(function (item) {
+		console.log(prices[item]);
+		text += ('<div class="datarow lead">' + typeToName[item.key] + '<span class="price">' + String(item.value.estimatedPrice) + '</span></div/>');
+	});
+	x.innerHTML = text;
 }
