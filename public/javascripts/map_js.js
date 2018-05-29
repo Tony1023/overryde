bbccd7fdee60b058a2073503f6c2dc2f0e8ef968
Eric Y <eye@usc.edu>
2018-05-28 22:46:23 -0700

    Altered 3 functions into Promises. Changed getLocation to locationPromise. Changed getHere into a leaner version called setWhere. Changed showLocationError to be more robust. Changed index.ejs to use Promises rather than multiple callbacks Created a new Error class called PossibleError.

--- public/javascripts/map_js.js
+++ public/javascripts/map_js.js
@@ -1,5 +1,19 @@
 import {EventTarget} from 'EventTarget.js'
 
+//Define custom error
+//Adapted from MDN Error code
+class PossibleError extends Error {
+  constructor(...params) {
+    // Pass remaining arguments (including vendor specific ones) to parent constructor
+    super(...params);
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, CustomError);
+    }
+  }
+}
+
 /*************************************************************/
 //FIND CURRENT POSITION
 /*************************************************************/
@@ -16,75 +30,68 @@ function showLocationError(error) {
   var x = document.getElementById("text_text");
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      x.innerHTML = "User denied the request for Geolocation."
-      break;
+    x.innerHTML = "User denied the request for Geolocation."
+    break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "Location information is unavailable."
-      break;
+    x.innerHTML = "Location information is unavailable."
+    break;
     case error.TIMEOUT:
-      x.innerHTML = "The request to get user location timed out."
-      break;
+    x.innerHTML = "The request to get user location timed out."
+    break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML = "An unknown error occurred."
-      break;
+    x.innerHTML = "An unknown error occurred."
+    break;
+    default:
+    x.innerHTML  = "Geolocation is not supported by this browser.";
+    throw Error("Geolocation unsupported");
   }
-  throw "Retry? Connection might be the issue.";
+  throw PossibleError("Retry");
 }
 
 //Get Current Location via HTML5
-function getLocation() {
-  var startpos = document.getElementById("map");
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position)=>{
-      //console.log(position);
-      //console.log(marker_score.start);
-      console.log("Init map");
-    }, (err)=>{
-      showLocationError(err);
-    });
-  } else {
-    let text_title = document.getElementById("text_title");
-    text_title.innerHTML = "ERROR: ";
-    let x = document.getElementById("text_text");
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+function locationPromise() {
+  return new Promise((resolve,reject)=>{
+    var startpos = document.getElementById("map");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position)=>{
+        //console.log(position);
+        //console.log(marker_score.start);
+        console.log("Init map");
+        resolve(position);
+      }, (err)=>{
+        showLocationError(err);
+      });
+    } else {
+      showLocationError({"code":"Geolocation Unavailible"});
+    }
+  });
 }
 
-function getHere(func){
-  if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    (position)=>{console.log(position);
-    origin_d = position.coords;
-    let here = {
-      lat: origin_d.latitude,
-      lng: origin_d.longitude
+//Set location from previous promise
+function setWhere(position){
+  return new Promise((resolve,reject)=>
+    {
+      console.log(position);
+      origin_d = position.coords;
+      let here = {
+        lat: origin_d.latitude,
+        lng: origin_d.longitude
+      };
+      console.log("Finished fetching", position.coords);
+      resolve(here);
     }
-    console.log("Finished fetching", position.coords);
-    mapRealInit(here);
-    },
-    (err)=>{showLocationError(err);}
-  );
-  } else {
-  let text_title = document.getElementById("text_title");
-  text_title.innerHTML = "ERROR: ";
-  let x = document.getElementById("text_text");
-  x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+  ));
 }
 /*************************************************************/
 // DRAW MAP
 /*************************************************************/
-function mapInit() {
-  getHere();
-}
-
-function mapRealInit(here) {
+function mapInit(here) {
   console.log(here);
   //init map
   //  if(typeof document.getElementById('map') !== 'object'){
   var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 4,
-  center: here
+    zoom: 4,
+    center: here
   });
   perm_map = map;
 
@@ -92,8 +99,8 @@ function mapRealInit(here) {
 
   map = perm_map;
   let marker1 = new google.maps.Marker({
-  map: map,
-  label: 'A'
+    map: map,
+    label: 'A'
   });
 
   marker1.setPosition(here);
@@ -108,32 +115,32 @@ function mapRealInit(here) {
   // bounds option in the request.
   //autocomplete.bindTo('bounds', map);
   var marker = new google.maps.Marker({
-  map: map,
-  label:'B'
+    map: map,
+    label:'B'
   });
 
   autocomplete.addListener('place_changed', function() {
-  marker.setVisible(false);
-  var place = autocomplete.getPlace();
-  //console.log(place);
-  if (!place.geometry) {
-    // User entered the name of a Place that was not suggested and
-    // pressed the Enter key, or the Place Details request failed.
-    window.alert("No details available for input: '" + place.name + "'");
-    return;
-  }
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    //console.log(place);
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
 
-  /*
-  // If the place has a geometry, then present it on a map.
-  if (place.geometry.viewport) {
+    /*
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
     map.fitBounds(place.geometry.viewport);
   } else {
 }
-  */
+*/
 
 
-  map.setCenter({lat:place.geometry.location.lat()-here.lat/2,
-    lng:place.geometry.location.lng()-here.lng/2});
+map.setCenter({lat:place.geometry.location.lat()-here.lat/2,
+  lng:place.geometry.location.lng()-here.lng/2});
   map.setZoom(17);  // Why 17? Because it looks good.
 
 
@@ -148,5 +155,5 @@ function mapRealInit(here) {
 
   target.dispatchEvent(cdocevent);
 
-  });
+});
 };
